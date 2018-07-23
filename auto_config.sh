ff0d3ebce6ee982bcdcbf5b4e322f25b04b57ee2
Xuhong Sun <sunxuhong1@gmail.com>
2018-07-23 20:01:19 +0800

    cover /etc/systemd/system/docker.service.d/http-proxy.conf

--- auto_config.sh
+++ auto_config.sh
@@ -5,11 +5,13 @@ export no_proxy=10.0.2.15,127.0.0.1
 mkdir -p /etc/systemd/system/docker.service.d
 touch /etc/systemd/system/docker.service.d/http-proxy.conf
 echo '[Service]
-Environment="HTTP_PROXY=https://www-proxy.ericsson.se:8080/"'>> /etc/systemd/system/docker.service.d/http-proxy.conf
+Environment="HTTP_PROXY=https://www-proxy.ericsson.se:8080/"'> /etc/systemd/system/docker.service.d/http-proxy.conf
 
 echo '[Service]
-Environment="HTTPS_PROXY=https://www-proxy.ericsson.se:8080/'>> /etc/systemd/system/docker.service.d/https-proxy.conf
+Environment="HTTPS_PROXY=https://www-proxy.ericsson.se:8080/'> /etc/systemd/system/docker.service.d/https-proxy.conf
 
 systemctl daemon-reload
 systemctl restart docker
 systemctl show --property=Environment docker
+
+docker pull nginx
